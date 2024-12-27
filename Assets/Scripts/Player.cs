63bbfe79bf3632daca603a99ccaf467028f66a6f
Lili <lili.berenyi@gmail.com>
2024-12-27 18:41:29 +0000

    Add prototype character controller using legacy input manager

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private void Update()
+    {
+        Vector2 inputVector = new();
+        if (Input.GetKey(KeyCode.W)) inputVector.y = 1;
+        if (Input.GetKey(KeyCode.S)) inputVector.y = -1;
+        if (Input.GetKey(KeyCode.A)) inputVector.x = -1;
+        if (Input.GetKey(KeyCode.D)) inputVector.x = 1;
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += Time.deltaTime * speed * moveDir;
+    }
+}
