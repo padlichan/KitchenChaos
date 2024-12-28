fbe35ae8d77e4a9d3aefdabdaaba66650f62c432
Lili <lili.berenyi@gmail.com>
2024-12-28 16:19:42 +0000

    Add rotation to character controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float turnSpeed;
     private void Update()
     {
         Vector2 inputVector = new();
@@ -15,5 +17,7 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += Time.deltaTime * speed * moveDir;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, turnSpeed * Time.deltaTime);
     }
 }
