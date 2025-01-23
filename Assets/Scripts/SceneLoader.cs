c521bcffa765d110f0e9757258c288a8da98b9ac
Lili <lili.berenyi@gmail.com>
2025-01-23 19:34:51 +0000

    Add main menu that loads game after a loading screen

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public enum Scene
+    {
+        MainMenuScene,
+        GameScene,
+        LoadingScene
+    }
+    private static Scene targetScene;
+
+    public static void Load(Scene targetScene)
+    {
+        SceneLoader.targetScene = targetScene;
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoaderCallback()
+    {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+}
---
+++ Assets/Scripts/SceneLoaderCallback.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SceneLoaderCallback : MonoBehaviour
+{
+    private bool isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (isFirstUpdate) isFirstUpdate = false;
+        SceneLoader.LoaderCallback();
+    }
+}
